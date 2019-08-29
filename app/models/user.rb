45614347d71f46a833497bfdd2939e73b0e1d556
key0123456789 <k.i-0123456789@live.jp>
2019-08-29 16:20:42 +0900

    Edit migration file
    ・ユーザー登録できるemailを一意に。
    ・バリデーションを設定。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 7 }
 end

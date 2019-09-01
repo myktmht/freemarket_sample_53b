5d05b7965ed405bf36465f52c75e2e6724e7b9ea
tom <miya4932@gmail.com>
2019-09-01 15:40:24 +0900

    git@example.comからgit@github.comに修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock "~> 3.11.0"
 set :application, 'freemarket_sample_53b'
 
 # どのリポジトリからアプリをpullするかを指定
-set :repo_url, "git@example.com:myktmht/freemarket_sample_53b.git"
+set :repo_url, "git@github.com:myktmht/freemarket_sample_53b.git"
 
 # バージョンbが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')

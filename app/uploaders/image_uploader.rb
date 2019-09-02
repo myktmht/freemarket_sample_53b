fa868c5805b40f07fd423821f3070b65f5345589
tom <miya4932@gmail.com>
2019-09-02 09:03:58 +0900

    image_uploaderに画像サイズの記述とcarrierwaveに画像保存先の条件分岐などを記載

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
+  process risize_to_fit: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?

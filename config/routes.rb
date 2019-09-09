01b227ef010d191b92c3c7b43fef5faabffb28ad
tom <miya4932@gmail.com>
2019-09-09 20:06:20 +0900

    テーブル関係とルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  get 'card/new'
-
-  get 'card/show'
 
     devise_for :users
     
     resources :users, only: [:show]
     resources :items, only: [:new, :create, :show]
+    resources :card, only: [:new, :show] do
+      collection do
+        get "paying"
+        post 'show', to: 'card#show'
+        post 'pay',  to: 'card#pay'
+        post 'delete', to: 'card#delete'
+      end
+    end
   
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root "tests#index"

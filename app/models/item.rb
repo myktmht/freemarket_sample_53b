0b5d46158dbf09b7f2f0cd87740c8eb707ce4431
katumoto <galaxyto1990@icloud.com>
2019-09-20 23:13:06 +0900

    create_models/brand
    brandモデルの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :size
   belongs_to :category
+  belongs_to :brand
   has_many :images
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190920134832) do
+ActiveRecord::Schema.define(version: 20190920135836) do
+
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false

62635b6a406f037e7197ac4660f0eb8333acbb41
katumoto <galaxyto1990@icloud.com>
2019-09-20 23:40:40 +0900

    create_index_action
    items/indexアクションの設定
    
    カテゴリー別にデータを参照し、最新順に１０件表示する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   
   def index
+    @ladyitems = get_items(1)
+    @manitems = get_items(2)
+    @cameraitems = get_items(8)
+    @hobyitems = get_items(6)
   end
   
   def new
@@ -12,4 +16,10 @@ class ItemsController < ApplicationController
   def show
   end
   
+
+  private
+  def get_items(id)
+    category = Category.find(id)
+    items = Item.where(category_id: category).order('id DESC').limit(10)
+  end
 end

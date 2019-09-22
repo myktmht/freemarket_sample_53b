2510acfbd710592b94e7eb6e2ef2badb436cc650
tom <miya4932@gmail.com>
2019-09-22 13:46:33 +0900

    クレジットカードテーブルをユーザーにネストさせて作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
     devise_for :users
     
-    resources :users, only: [:show]
+    resources :users, only: [:show] do
+      resources :card, only: [:new]
+    end
     resources :items, only: [:new, :create, :show]
-    resources :card, only: [:new]
     resources :mypage_card, only: [:index, :new]
   
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

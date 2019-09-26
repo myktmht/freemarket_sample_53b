e5a6ea70fc7d7bb3cd26314794900b73de568128
tom <miya4932@gmail.com>
2019-09-26 17:03:28 +0900

    クレジットカード登録・削除機能を実装

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -21,7 +21,7 @@ class CardController < ApplicationController
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
       )
-      @card = Card.new(user_id: current_user_id, customer_id: customer.id, card_id: customer.default_card)
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "show"
       else
@@ -39,7 +39,7 @@ class CardController < ApplicationController
       customer.delete
       card.delete
     end
-      # redirect_to Paying_users_path
+    redirect_to action: "paying"
   end
 
   def show

6b3187bd9701624b81d86541e3a07f66a2d1d850
katumoto <galaxyto1990@icloud.com>
2019-09-26 22:33:13 +0900

    item/indexのMVC作成
    item/indexにおけるMVC設定
    
    ・ルーティング設定
    ・ビュー作成
    ・indexアクションの定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
     devise_for :users
     
     resources :users, only: [:show]
-    resources :items, only: [:new, :create, :show]
+    resources :items, only: [:index, :new, :create, :show]
     resources :card, only: [:new]
     resources :mypage_card, only: [:index, :new]
   
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root "tests#index"
+    root "items#index"
   
   end

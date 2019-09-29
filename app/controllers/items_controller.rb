4927682c7b13f3897118ee6749454ff731ef1e6f
katumoto <galaxyto1990@icloud.com>
2019-09-29 15:10:02 +0900

    create_index_action
    items/indexアクションの設定
    
    カテゴリー別にデータを参照し、最新順に１０件表示する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,12 @@
 class ItemsController < ApplicationController
 
+  def index
+    @ladyitems = get_items(1)
+    @manitems = get_items(199)
+    @cameraitems = get_items(892)
+    @hobyitems = get_items(679)
+  end
+
   def new
     @item = Item.new
     @category1 = Category.eager_load(children: {children: :children}).where(parent_id: 0) 
@@ -17,4 +24,8 @@ class ItemsController < ApplicationController
     prams.require(:item).permit(:name, :price, :description, :condition, :shipping_fee, :days_before_shipping, :shipping_method, :trade_status).merge(user_id: current_user.id)
   end
 
+  def get_items(id)
+    category = Category.find(id)
+    items = Item.where(category_id: category).order('id DESC').limit(10)
+  end
 end

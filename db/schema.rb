a861e02aa1c6a70e3bea5142a459b696313941d9
katumoto <galaxyto1990@icloud.com>
2019-09-29 13:12:42 +0900

    AddColumn_users_deleted_at
    usersテーブルに、deleted_atカラムを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190926150851) do
+ActiveRecord::Schema.define(version: 20190929035528) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20190926150851) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree

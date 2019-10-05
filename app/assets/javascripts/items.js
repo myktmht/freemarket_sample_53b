261e49c6dffdbc0122e8ec102549c2ab5d6e9c8d
key0123456789 <k.i-0123456789@live.jp>
2019-10-06 01:35:25 +0900

    Update items.js
    ・console.logの削除
    ・行間調整

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -5,15 +5,11 @@ $(function(){
     if (Math.sign(input) === 1) {
       var price_commission = Math.floor(input * 0.1).toString().replace(/(\d)(?=(\d{3})+$)/g , '$1,');
       var price_profit = (input - Math.floor(input * 0.1)).toString().replace(/(\d)(?=(\d{3})+$)/g , '$1,');
-      console.log(input);
       $('#commission').text('\xA5' + price_commission);
       $('#profit').text('\xA5' + price_profit);
-
-      } else {
-
+    } else {
       $('#commission').text('-');
       $('#profit').text('-');
-
     }
   });
 });

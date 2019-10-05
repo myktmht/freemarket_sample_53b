078a83b4645051bda91a9584344d0b3fa1fcabf9
key0123456789 <k.i-0123456789@live.jp>
2019-10-06 01:38:26 +0900

    add deleted_at to users
    ・論理削除に対応するため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190929041428) do
+ActiveRecord::Schema.define(version: 20190929050522) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20190929041428) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree

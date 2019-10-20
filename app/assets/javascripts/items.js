449ba7394a460998f379262e70bbe0a4e57534ad
key0123456789 <k.i-0123456789@live.jp>
2019-10-20 15:23:58 +0900

    Fix comment

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,24 +1,36 @@
 $(document).on('turbolinks:load', $(function(){
 
-  var upload_images =  $("#sell-images-area");
+  // // 出品画像
+  // var upload_images =  $("#sell-images-area");
 
-  function addImageField(){
-    var html = `<div class="sell-content__image">
-                  <input type="file" name="item[image][filename]" id="item_image_filename">
-                </div>`
+  // function addImageField(i){
+  //   var html = `<div class="sell-content__image">
+  //                 <input type="file" name="image[filename][]" id="item_image_filename">
+  //               </div>`
 
-    upload_images.append(html);
-  }
+  //   upload_images.append(html);
+  // }
 
-  upload_images.on("change", function(){
-    var lastImage = $('input[type="file"]:last').prop('files')[0]
-    if(lastImage && $('input[type="file"]').length < 10){
-      addImageField();
-    } else {
-      return false;
-    }
-  });
+  // var i = 0;
+  // upload_images.on("change", function(){
+  //   var lastImage = $('input[type="file"]:last').prop('files')[0]
+  //   if(lastImage && $('input[type="file"]').length < 10){
+  //     i++
+  //     addImageField(i);
+  //   } else {
+  //     return false;
+  //   }
+  // });
 
+  // // 商品の詳細＞カテゴリー
+  // $("#category0").on("change", function(){
+  // // 大カテゴリー(0)選択時の値を取得
+  //   var category0_val = $("option:selected").val();
+  // // 中カテゴリー(1)選択時の値を取得
+  //   var category1_val = $("option:selected").val();
+  // });
+
+  // 販売価格
   $('#item_price').on('keyup', function(){
     var input = $(this).val();
 

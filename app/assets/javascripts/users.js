603c2c22f5011057218cd1003149a29a641a9703
Kuki1204 <okknjp1204@yahoo.co.jp>
2019-10-20 15:31:27 +0900

    Add Jquery

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,23 @@
+
+jQuery(function($){
+  $('.tab').click(function(){
+    console.log('.is-active');
+      $('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+      $('.is-show').removeClass('is-show');
+      // クリックしたタブからインデックス番号を取得
+      const index = $(this).index();
+      // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+      $('.panel').eq(index).addClass('is-show');
+  });
+  $('.tab2').click(function(){
+    console.log('.is-active2');
+      $('.is-active2').removeClass('is-active2');
+      $(this).addClass('is-active2');
+      $('.is-show2').removeClass('is-show2');
+      // クリックしたタブからインデックス番号を取得
+      const index = $(this).index();
+      // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+      $('.panel2').eq(index).addClass('is-show2');
+  });
+});

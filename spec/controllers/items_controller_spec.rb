1d1732fba9e08e301a8efcc5edaf36692c786f6c
key0123456789 <k.i-0123456789@live.jp>
2019-10-27 15:03:14 +0900

    Add test for item registration #create

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -66,6 +66,84 @@ describe ItemsController do
         expect(response).to redirect_to "/login"
       end
     end
+  end
 
+  describe 'POST #create'do
+    let(:item) { create(:item) }
+    
+    before do
+      @user = FactoryBot.create(:user)
+    end
+
+    it 'is valid with colums' do
+      item = build(:item)
+      expect(item).to be_valid
+    end
+
+    it 'is invalid without name' do
+      item = build(:item, name: nil)
+      item.valid?
+      expect(item.errors[:name])
+    end
+
+    it 'is invalid without price' do
+      item = build(:item, price: nil)
+      item.valid?
+      expect(item.errors[:price])
+    end
+
+    it 'is invalid without description' do
+      item = build(:item, description: nil)
+      item.valid?
+      expect(item.errors[:description])
+    end
+
+    it 'is invalid without condition' do
+      item = build(:item, condition: nil)
+      item.valid?
+      expect(item.errors[:condition])
+    end
+
+    it 'is invalid without shipping_fee' do
+      item = build(:item, shipping_fee: nil)
+      item.valid?
+      expect(item.errors[:shipping_fee])
+    end
+
+    it 'is invalid without shipping_from' do
+      item = build(:item, shipping_from: nil)
+      item.valid?
+      expect(item.errors[:shipping_from])
+    end
+
+    it 'is invalid without days_before_shipping' do
+      item = build(:item, days_before_shipping: nil)
+      item.valid?
+      expect(item.errors[:days_before_shipping])
+    end
+
+    it 'is invalid without shipping_method' do
+      item = build(:item, shipping_method: nil)
+      item.valid?
+      expect(item.errors[:shipping_method])
+    end
+
+    it 'is invalid without trade_status' do
+      item = build(:item, trade_status: nil)
+      item.valid?
+      expect(item.errors[:trade_status])
+    end
+
+    it 'is invalid without user_id' do
+      item = build(:item, user_id: nil)
+      item.valid?
+      expect(item.errors[:user_id])
+    end
+
+    it 'is invalid without category_id' do
+      item = build(:item, category_id: nil)
+      item.valid?
+      expect(item.errors[:category_id])
+    end
   end
 end

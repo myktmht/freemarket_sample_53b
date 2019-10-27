64f9e17d63ef436721193fa005448c3f3f6f62fd
tom <miya4932@gmail.com>
2019-10-27 14:09:54 +0900

    addressテーブルの作成とアソシエーションを結ぶ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
            :recoverable, :rememberable, :validatable
     has_many :cards
     has_one :profile
+    has_one :address
   
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :nickname, presence: true

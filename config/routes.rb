eba5567f5855ad4678dc44d5777fe2490b83071a
key0123456789 <k.i-0123456789@live.jp>
2019-11-01 23:23:50 +0900

    Delete get '/users/index'

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
 
-  get '/users/index', to: 'users#index'
   resources :users, only: [:index, :show, :new] do
     collection do
 

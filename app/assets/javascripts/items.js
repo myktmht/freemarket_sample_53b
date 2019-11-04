e90349a487448db8010e14d0b6facfa3ffcf66ae
key0123456789 <k.i-0123456789@live.jp>
2019-11-04 22:28:20 +0900

    Delete Preview(temporarliy saved)

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,5 +1,55 @@
 $(document).on('turbolinks:load', function(){
 
+  $(function(){
+    //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
+    // $('form').on('change', 'input[type="file"]', function(e) {
+    //   var file = e.target.files[0],
+    //       reader = new FileReader(),
+    //       $preview = $("#preview");
+    //       t = this;
+
+    //   var image_preview = $("#preview");
+
+    //   function appendPreview(img){
+    //     console.log(img);
+    //     var html = `<li class="preview">
+    //                   <div class="preview-img">
+    //                     ${ img }
+    //                   </div>
+    //                   <div class="preview-button">
+    //                     <a href="" class="preview-button-delete">削除</a>
+    //                   </div>
+    //                 </li>`
+
+    //     image_preview.append(html)
+    //   }
+
+    //   // 画像ファイル以外の場合は何もしない
+    //   if(file.type.indexOf("image") < 0){
+    //     return false;
+    //   }
+  
+    //   // ファイル読み込みが完了した際のイベント登録
+    //   reader.onload = (function(file) {
+    //     return function(e) {
+    //       // .prevewの領域の中にロードした画像を表示するimageタグを追加
+    //       var img = $('<img>').attr({
+    //         src: e.target.result,
+    //         width: "150px",
+    //         class: "preview",
+    //         title: file.name
+    //       })
+    //       appendPreview(img);
+    //     };
+    //   })
+    // });
+
+    // previewの削除
+    $(document).on('click', '.preview-button-delete', function() {
+      $(this).parent().parent().remove();
+    })
+  });
+
   // // カテゴリーの絞り込み(要検討)
   // // 商品の詳細＞カテゴリー
   // $("#category0").on("change", function(){

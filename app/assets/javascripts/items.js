0df296894b1aea4d528587a17aaa05f626ee8084
key0123456789 <k.i-0123456789@live.jp>
2019-11-07 01:50:39 +0900

    Active button

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -29,4 +29,30 @@ $(document).on('turbolinks:load', function(){
       $('#profit').text('-');
     }
   });
+
+  $(function(){
+    function toggleChangeBtn() {
+      var slideIndex = $('.slide').index($('.active'));/*表示中のスライドのindexを取得*/
+      $('.button').show();/*両ボタンを表示*/
+      if(slideIndex == 0){/*一番最初の要素が表示されているとき*/
+        $('.prev').hide();/*prevボタンを隠す。*/
+      }else if(slideIndex == $('.slide').length - 1){/*一番最後の要素が表示されているとき*/
+        $('.next').hide();/*nextボタンを隠す。*/
+      }
+    }
+    toggleChangeBtn();
+
+    $('.next').click(function() {
+        var $displaySlide = $('.active');
+        $displaySlide.removeClass('active');
+        $displaySlide.next().addClass('active');
+        toggleChangeBtn();
+    });
+    $('.prev').click(function() {
+        var $displaySlide = $('.active');
+        $displaySlide.removeClass('active');
+        $displaySlide.prev().addClass('active');
+        toggleChangeBtn();
+    });
+  });
 });

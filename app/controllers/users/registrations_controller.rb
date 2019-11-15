3235c585b5a43b3af51d40394da7b67dc701a9c6
key0123456789 <k.i-0123456789@live.jp>
2019-11-15 10:46:43 +0900

    Insert conditional branch for reCaptcha in registrations_controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :authenticate_user!
+  prepend_before_action :check_captcha, only: [:create]
+  prepend_before_action :customize_sign_up_params, only: [:create]
   
-#   def mypage
-#   end
- 
-#   protected
- 
   def after_sign_up_path_for(resource)
      guide_beginner_path
+  end
+
+  private
+  def customize_sign_up_params
+    devise_parameter_sanitizer.permit :sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me]
+  end
+
+  def check_captcha
+    self.resource = resource_class.new sign_up_params
+    resource.validate
+    unless verify_recaptcha(model: resource)
+      flash.now[:recaptcha_error] = I18n.t('recaptcha.errors.verification_failed')
+      respond_with_navigational(resource) { render :new }
+    end
   end
 end
